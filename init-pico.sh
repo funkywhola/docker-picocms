44c24865f1a69773541b6007f468cc77420378c4
ed.tsai <ed.tsai@neurv.com>
2022-01-17 12:12:01 +0800

    wget instead of curl

--- init-pico.sh
+++ init-pico.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir data
-curl -O https://github.com/picocms/Pico/releases/download/v2.1.4/pico-release-v2.1.4.tar.gz
+wget https://github.com/picocms/Pico/releases/download/v2.1.4/pico-release-v2.1.4.tar.gz
 tar -zxvf pico-release-v2.1.4.tar.gz -C data/
 docker-compose up -d
